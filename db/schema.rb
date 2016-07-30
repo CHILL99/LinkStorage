5d728f412960e131e49a686614e19557881cb3ab
chill99 <rost.chill99@gmail.com>
2016-07-30 16:50:28 +0300

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -25,19 +25,19 @@ ActiveRecord::Schema.define(version: 20160730111833) do
     t.datetime "updated_at",  null: false
   end
 
-    create_table "tags", force: :cascade do |t|
+  create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
