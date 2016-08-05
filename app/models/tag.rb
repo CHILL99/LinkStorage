215091a1bc3dc9d709623ce44a46e6f041ba187e
chill99 <rost.chill99@gmail.com>
2016-08-05 18:59:49 +0300

    Refactor tag.rb 2

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
   has_many :bookmark_tag_mappings
   has_many :bookmarks, through: :bookmark_tag_mappings
 
-  def user_tags(user)
+  def self.user_tags(user)
     user.bookmarks.map{|bookmark| bookmark.all_tags}.join(", ")
   end
 end
