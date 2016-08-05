99b7710a157c57ef5d522341a677835a0a6c99ba
chill99 <rost.chill99@gmail.com>
2016-08-05 11:46:40 +0300

    Add admin panel, modify routes

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'simple_form'
 gem 'haml-rails'
 gem 'devise'
+gem 'rails_admin'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_scope :user do
     authenticated :user do
       root 'bookmarks#index', as: :authenticated_root
