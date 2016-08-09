72989e0d63ea9bd3f5d3691ebfd0a6e9d8e34add
chill99 <rost.chill99@gmail.com>
2016-08-09 19:26:57 +0300

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  FLASH_TYPE = { alert: "danger", notice: "success"}
+
+  def flash_message
+    flash.map do |key, msg|
+      content_tag :div, :class => "alert alert-#{FLASH_TYPE[key.to_sym] || key } alert-dismissible", :role => "alert" do
+        concat (content_tag :button, :type => "button", :class => "close", data:{:dismiss => "alert"}, aria:{:label => "Close"} do
+          content_tag :span, "&times;".html_safe, aria:{:hidden => "true"}
+        end)
+        concat msg
+      end
+    end.join.html_safe
+  end
 end
