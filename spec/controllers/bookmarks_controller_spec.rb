10a55903b82e8e4f5ae1b708c8aa53e084e8ded1
chill99 <rost.chill99@gmail.com>
2016-08-09 19:01:16 +0300

    Add test of update action of bookmarks controller

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -10,25 +10,57 @@ RSpec.describe BookmarksController do
   end
 
   describe '#index' do
-    it 'should get index' do
+    it 'gets index' do
       get 'index'
       expect(response).to render_template("index")
     end
   end
   
   describe '#new' do
-    it 'should render new form' do
+    it 'renders new form' do
       get 'new'
       expect(response).to be_success
     end
   end
 
+  describe '#create' do
+    context 'with valid params' do
+      let(:params) { { content: Faker::Internet.url,
+                       description: Faker::Lorem.paragraph,
+                       } }
+
+      before(:each) do
+        post :create, bookmark: params
+      end
+
+      it 'redirect to bookmarks index page' do
+        expect(response).to redirect_to(authenticated_root_path)
+      end
+
+      it 'creates new bookmark' do
+        expect { post :create, bookmark: params }.to change(Bookmark, :count).by(1)
+      end
+    end
   
+    context 'with invalid params' do
+      let(:params) { {description: Faker::Lorem.paragraph } }
+
+      before(:each) do
+        post :create, bookmark: params
+      end
+
+      it 'render form again' do
+        expect(response).to render_template('bookmarks/new')
+      end
+    end
+  end
+
   describe '#edit' do
-    it 'should render form' do
+    it 'renders form' do
+      sign_in(bookmark.user)
       get 'edit', { id: bookmark.id }
       expect(response).to be_success
     end
   end
-
+  
 end
